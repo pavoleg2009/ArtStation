25d3d4027bfe06b29a30409cfd01ff05df6ffa75
Oleg Pavlichenkov <oleg.pavlichenkov@flatstack.com>
2018-05-17 13:05:47 +0300

    Initial commit

---
+++ ArtStationClassic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArtStationClassic
+//
+//  Created by Oleg Pavlichenkov on 17/05/2018.
+//  Copyright © 2018 Oleg Pavlichenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
